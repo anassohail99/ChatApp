9af5f50876b2c32c7c28fec0a16be89dfc6be484
= <=>
2020-06-03 00:52:45 +0500

    Final Version main js file

--- public/js/main.js
+++ public/js/main.js
@@ -1,9 +1,28 @@
 const chatForm = document.getElementById("chat-form");
 const chat = document.querySelector(".chat-messages");
+const roomName = document.getElementById("room-name");
+const userList = document.getElementById("users");
+
+// get userName and room from url
+const { username, room } = Qs.parse(location.search, {
+  ignoreQueryPrefix: true
+});
+
+// console.log(username, room);
+
 const socket = io();
 
+// joined chatRoom
+socket.emit("joinRoom", { username, room });
+
+//get room Users
+socket.on("roomUsers", ({ room, users }) => {
+  outputRoomName(room);
+  outputRoomUsers(users);
+});
+
 // Message from server
-socket.on("message", (message) => {
+socket.on("message", message => {
   console.log(message);
   outPutMessage(message);
 
@@ -12,7 +31,7 @@ socket.on("message", (message) => {
 });
 
 // Message submit
-chatForm.addEventListener("submit", (e) => {
+chatForm.addEventListener("submit", e => {
   e.preventDefault();
 
   const msg = e.target.elements.msg.value;
@@ -22,7 +41,7 @@ chatForm.addEventListener("submit", (e) => {
 
   //clear input
   e.target.elements.msg.value = "";
-  e.target.elements.msg.value.focus();
+  e.target.elements.msg.focus();
 });
 
 // output message to dom
@@ -35,4 +54,22 @@ function outPutMessage(message) {
   </p>
   `;
   document.querySelector(".chat-messages").appendChild(div);
+}
+
+// Add room name to dom
+function outputRoomName(room) {
+  roomName.innerText = room;
+}
+
+// Add users to dom
+function outputRoomUsers(users) {
+  userList.innerHTML = `
+    ${users
+      .map(
+        user => `<li>
+      ${user.username}
+    </li>`
+      )
+      .join("")}
+  `;
 }
